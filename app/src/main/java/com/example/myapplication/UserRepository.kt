e16d53675f8e9701f4b8942adcdc396ca6fc8bf7
mashaler <mashalerolva@gmail.com>
2023-05-15 16:11:26 +0200

    updated

---
+++ app/src/main/java/com/example/myapplication/UserRepository.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import retrofit2.Response
+
+class UserRepository {
+    suspend fun loginUser(loginRequest:LoginRequest):
+            Response<LoginResponse>? {
+            return UserApi.getApi()?.loginUser(loginRequest =
+            loginRequest)
+    }
+}
